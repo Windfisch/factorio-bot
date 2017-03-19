fc6da29d0b23c82a49d228242c4adf6a41dfc22d
Florian Jung <flo@windfisch.org>
2017-03-20 00:55:46 +0100

    refactor, gui

--- factorio_io.cpp
+++ factorio_io.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
 #include <stdexcept>
 #include <unistd.h>
 #include <cstdio>
@@ -9,8 +8,7 @@
 #include <set>
 #include <deque>
 
-#include "worldmap.hpp"
-#include "pos.hpp"
+#include "factorio_io.h"
 #include "gui/gui.h"
 
 #define READ_BUFSIZE 1024
@@ -18,100 +16,11 @@
 using std::string;
 using std::unordered_map;
 
-enum dir4_t
-{
-	NORTH=0,
-	EAST,
-	SOUTH,
-	WEST
-};
-
-struct Area
-{
-	Pos left_top;
-	Pos right_bottom;
-
-	Area() {}
-	Area(int x1, int y1, int x2, int y2) : left_top(x1,y1), right_bottom(x2,y2)  {}
-	Area(const Pos& lt, const Pos& rb) : left_top(lt), right_bottom(rb) {}
-	Area(std::string str);
-
-	std::string str() const;
-
-	bool contains(const Pos& p) const { return left_top <= p && p < right_bottom; }
-};
-
-constexpr int NOT_YET_ASSIGNED = -1; // TODO FIXME
-
-struct Resource
-{
-	enum type_t
-	{
-		NONE,
-		COAL,
-		IRON,
-		COPPER,
-		STONE,
-		OIL
-	};
-	static const unordered_map<string, Resource::type_t> types;
-
-	enum floodfill_flag_t
-	{
-		FLOODFILL_NONE,
-		FLOODFILL_QUEUED
-	};
-
-	floodfill_flag_t floodfill_flag = FLOODFILL_NONE;
-	
-	type_t type;
-	int patch_id;
-
-	Resource(type_t t, int parent) : type(t), patch_id(parent) {}
-	Resource() : type(NONE), patch_id(NOT_YET_ASSIGNED) {}
-
-};
-const unordered_map<string, Resource::type_t> Resource::types = { {"coal", COAL}, {"iron-ore", IRON}, {"copper-ore", COPPER}, {"stone", STONE}, {"crude-oil", OIL} };
-
-class FactorioGame
-{
-	private:
-		std::string line_buffer;
-
-		std::string factorio_file_prefix;
-		std::ifstream factorio_file;
-		int factorio_file_id = 1;
-
-		std::string rcon_host;
-		int rcon_port;
-
-		void change_file_id(int new_id);
-		std::string factorio_file_name();
-		std::string remove_line_from_buffer();
-
-		void parse_tiles(const Area& area, const string& data);
-		void parse_resources(const Area& area, const string& data);
-
-		int next_free_resource_id = 0;
-	
-	public:
-		FactorioGame(std::string prefix, std::string rcon_host, int rcon_port);
-		std::string read_packet();
-		void parse_packet(const std::string& data);
-		void floodfill_resources(const WorldMap<Resource>::Viewport& view, const Area& area, int x, int y, int radius);
-
-		struct walk_t
-		{
-			bool can_walk;
-			bool can_cross;
-			bool corridor[4];
-		};
-
-		WorldMap<walk_t> walk_map;
-		WorldMap<Resource> resource_map;
-};
 
 using namespace std;
+
+const unordered_map<string, Resource::type_t> Resource::types = { {"coal", COAL}, {"iron-ore", IRON}, {"copper-ore", COPPER}, {"stone", STONE}, {"crude-oil", OIL} };
+
 
 Area::Area(string str)
 {
@@ -318,7 +227,7 @@ int main()
 	FactorioGame factorio("/home/flo/kruschkram/factorio-AImod/script-output/output", "localhost", 1234);
 	int i=0;
 
-	GUI::MapGui gui(NULL);
+	GUI::MapGui gui(&factorio);
 
 	while (true)
 	{
--- gui/gui.cpp
+++ gui/gui.cpp
@@ -4,9 +4,30 @@
 #include <FL/Fl_Box.H>
 
 #include "gui.h"
+#include "../factorio_io.h"
+#include <cmath>
+
+using std::floor;
+using std::sin;
 
 namespace GUI
 {
+
+class MapBox : public Fl_Box
+{
+       protected:
+               virtual void draw(void);
+       private:
+               std::vector<unsigned char> imgbuf;
+	       int imgwidth, imgheight;
+	       std::unique_ptr<Fl_RGB_Image> rgbimg;
+	       const FactorioGame* game;
+
+       public:
+               MapBox(const FactorioGame* game, int X, int Y, int W, int H, const char *L=0);
+	       void update_imgbuf();
+};
+
 
 class _MapGui_impl
 {
@@ -15,30 +36,22 @@ class _MapGui_impl
 
 	private:
 		std::unique_ptr<Fl_Window> window;
-		std::unique_ptr<Fl_Box> mapbox;
-};
+		std::unique_ptr<MapBox> mapbox;
 
-class MapBox : public Fl_Box
-{
-       protected:
-               virtual void draw(void);
-       private:
-               std::vector<unsigned char> imgbuf;
-	       std::unique_ptr<Fl_RGB_Image> rgbimg;
-
-       public:
-               MapBox(int X, int Y, int W, int H, const char *L=0);
+		const FactorioGame* game;
 };
 
 void MapBox::draw(void)
 {
-	rgbimg->draw(x(),y());
+	rgbimg->draw(x(),y(),w(),h());
 }
 
-MapBox::MapBox(int x, int y, int w, int h, const char* l) : Fl_Box(x,y,w,h,l)
+MapBox::MapBox(const FactorioGame* g, int x, int y, int w, int h, const char* l) : Fl_Box(x,y,w,h,l), game(g)
 {
-	imgbuf.resize(4*1000*1000);
-	for (int i=0; i<1000*1000; i++)
+	imgwidth = 1000;
+	imgheight = 1000;
+	imgbuf.resize(4*imgwidth*imgheight);
+	for (int i=0; i<imgwidth*imgheight; i++)
 	{
 		uint32_t foo = i*16;
 		imgbuf[4*i+0] = foo & 0xff;
@@ -47,9 +60,59 @@ MapBox::MapBox(int x, int y, int w, int h, const char* l) : Fl_Box(x,y,w,h,l)
 		imgbuf[4*i+3] = 255;
 	}
 
-	rgbimg.reset(new Fl_RGB_Image(imgbuf.data(), 1000, 1000, 4));
+	rgbimg.reset(new Fl_RGB_Image(imgbuf.data(), imgwidth, imgheight, 4));
 }
 
+struct Color {
+	int r,g,b;
+	Color(int rr,int gg,int bb):r(rr),g(gg),b(bb){}
+};
+
+Color color_hsv(double hue, double sat, double val)
+{
+	double r,g,b;
+	int i = int(floor(hue));
+	double f = hue * 6 - i;
+	int p = int(255 * val * (1 - sat));
+	int q = int(255 * val * (1 - f * sat));
+	int t = int(255 * val * (1 - (1 - f) * sat));
+	int v = int(255 * val);
+
+	switch ((i%6+6)%6)
+	{
+		case 0: return Color(v,t,p);
+		case 1: return Color(q,v,p);
+		case 2: return Color(p,v,t);
+		case 3: return Color(p,q,v);
+		case 4: return Color(t,p,v);
+		case 5: return Color(v,p,q);
+	}
+}
+
+Color get_color(int id)
+{
+	if (id==0) return Color(0,0,0);
+	return color_hsv(0.53481*id, 0.8 + 0.2*sin(id/21.324), 0.7+0.3*sin(id/2.13184));
+}
+
+static MapBox* foo = 0;
+
+void MapBox::update_imgbuf()
+{
+	auto view = game->resource_map.view(Pos(-500,-500),Pos(500,500),Pos(-500,-500));
+	for (int x=0; x<imgwidth; x++)
+		for (int y=0; y<imgheight; y++)
+		{
+			int idx = (y*imgwidth+x)*4;
+			Color col = get_color(view.at(x,y).patch_id);
+			imgbuf[idx+0] = col.r;
+			imgbuf[idx+1] = col.g;
+			imgbuf[idx+2] = col.b;
+			imgbuf[idx+3] = 255;
+		}
+
+	rgbimg.reset(new Fl_RGB_Image(imgbuf.data(), imgwidth, imgheight, 4));
+}
 
 MapGui::MapGui(const FactorioGame* game) : impl(new _MapGui_impl(game)) {}
 MapGui::~MapGui() {}
@@ -57,17 +120,23 @@ MapGui::~MapGui() {}
 _MapGui_impl::_MapGui_impl(const FactorioGame* game)
 {
 	window.reset(new Fl_Window(340,180));
-	mapbox.reset(new Fl_Box(20,40,300,100,"Hello, World!"));
+	mapbox.reset(new MapBox(game,20,40,300,100,"Hello, World!"));
 	mapbox->box(FL_UP_BOX);
 	mapbox->labelfont(FL_BOLD+FL_ITALIC);
 	mapbox->labelsize(36);
 	mapbox->labeltype(FL_SHADOW_LABEL);
+	foo = mapbox.get(); // FIXME
+	window->resizable(mapbox.get());
 	window->end();
 	window->show();
+
+	this->game = game;
 }
 
 double GUI::wait(double t)
 {
+	static int cnt = 0;
+	if (cnt++ % 100 == 0) foo->update_imgbuf();
 	return Fl::wait(t);
 }
 
