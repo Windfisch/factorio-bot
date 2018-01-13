9891a82d079ad8af668d4bcee241d01dcfd9a6e9
Florian Jung <flo@windfisch.org>
2018-01-13 20:19:33 +0100

    missing mine_planning.h

---
+++ mine_planning.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include "pos.hpp"
+#include "entity.h"
+
+class FactorioGame;
+
+std::vector<PlannedEntity> plan_mine(const std::vector<Pos>& positions, Pos destination, const FactorioGame& game);
+std::vector<PlannedEntity> plan_mine(const std::vector<Pos>& positions, Pos destination, unsigned side_max, const EntityPrototype* belt_proto, const EntityPrototype* machine_proto);
