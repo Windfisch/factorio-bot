949a96e544b60b08ecfbe0c11f6f43ce9d7fcd7b
Florian Jung <flo@windfisch.org>
2018-11-29 00:07:33 +0100

    filter.sh also logs

--- filter.sh
+++ filter.sh
@@ -1,1 +1,1 @@
-grep --line-buffered -ve '^\S*\(dump\|detail\|floodfill_resources\|pathfinding\|verbose\|action_registry\|core\.objects\|calculate_schedule\)\S*:' | sed 's/^\S*: //'
+tee log.txt | grep --line-buffered -ve '^\S*\(dump\|detail\|floodfill_resources\|pathfinding\|verbose\|action_registry\|core\.objects\|calculate_schedule\)\S*:' | sed 's/^\S*: //'
